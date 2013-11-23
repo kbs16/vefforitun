20864caeab075478b17e7b783b7c4f7c195a9859
Alfred Simonarson <als34@hi.is>
2013-11-23 14:51:50 +0000

    marker kominn á rvk

--- googlemaps.php
+++ googlemaps.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="is">
   <head>
+    <meta charset="UTF-8">
     <style>
       #map_canvas {
-        width: 500px;
-        height: 400px;
+        width: 600px;
+        height: 500px;
       }
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
     <script>
       function initialize() {
+        var rvkLatlng = new google.maps.LatLng(64.1330,-21.9330);
         var map_canvas = document.getElementById('map_canvas');
         var map_options = {
-          center: new google.maps.LatLng(64.1330, -21.9330),
+          center: new google.maps.LatLng(rvkLatlng),
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(map_canvas, map_options)
+
+        var marker = new google.maps.Marker({
+          position: rvkLatlng,
+          title:"Halló Reykjavík!"
+      });
       }
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
