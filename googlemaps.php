2d1c6743db8a8d72690529307b178e03413c852b
kbs16 <kbs16@hi.is>
2013-11-29 17:11:31 +0000

    lagfæring á css og php

--- googlemaps.php
+++ googlemaps.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="is">
-  <head>
-    <meta charset="UTF-8">
-    <style>
-      #map_canvas {
-        width: 600px;
-        height: 500px;
-      }
-    </style>
+
     <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
     <script src="./googlemaps.js"></script>
-  </head>
-  <body>
+
     <div id="map_canvas"></div>
-  </body>
-</html>
